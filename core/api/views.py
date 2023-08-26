173782dc5135575e830a8aebfd5a3e0cf7d25b72
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-26 11:30:58 +0000

    create an api overview endpoint

--- core/api/views.py
+++ core/api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+# import restapi essentials
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
+
+
+class OverviewAPI(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            "message": "The API for TickTract is running successfully",
+            "endpoints": [
+                {"endpoint": "/", "description": "returns the overview of the entire api infrastructure"},
+            ]
+        })
