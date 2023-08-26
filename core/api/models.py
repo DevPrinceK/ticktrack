66b8a50f369410c340d3350dd0021d648fe40415
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-26 11:29:55 +0000

    create a custom user model

--- core/api/models.py
+++ core/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import AccountManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    fullname = models.CharField(max_length=100, blank=True, null=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def get_fullname(self):
+        '''return the full name of the user'''
+        return self.fullname if self.fullname else self.email if self.email else 'Anonymous'  # noqa
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.get_fullname()
+
+    class Meta:
+        db_table = 'user'
