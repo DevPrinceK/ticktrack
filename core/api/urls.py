ce4d1ce92b475aa9a6332cb59985ac115ade7161
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-26 12:21:36 +0000

    Update urls.py

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from knox import views as knox_views
+
 from . import views
 
 app_name = "api"
 
 urlpatterns = [
-    path("", views.OverviewAPI.as_view(), name="overview")
+    path("", views.OverviewAPI.as_view(), name="overview"),
+    path("login/", views.LoginAPI.as_view(), name="login"),
+        # logout user
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    # logout user from all sessions
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
